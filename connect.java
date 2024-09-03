fa99389b8066a75afc039fd1a095b875e384cfb7
raju084 <142029270+raju084@users.noreply.github.com>
2024-09-04 00:22:02 +0530

    Update connect.java

--- connect.java
+++ connect.java
@@ -1,5 +1,14 @@
-package project;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-public class connect {
-
+public class Connect {
+    public static Connection connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://localhost/mysql", "root", "edureka");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
